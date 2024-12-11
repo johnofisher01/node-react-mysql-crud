4dd21e69ea5d3d421aa5ac70aebebadf4db3e9e5
Johno <john@mount-media-online.com>
2024-12-11 14:04:42 +0000

    sass/bootstrap

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,44 +1,58 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Table, Button, Container } from "react-bootstrap";
 
 const Home = () => {
-  const [data, setdata] = useState([]);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:8081/")
-      .then((res) => setdata(res.data))
+      .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
+
   return (
-    <>
-      <table>
-        <thead>
+    <Container className="mt-4">
+      <h1 className="mb-4">Student Records</h1>
+      <Table striped bordered hover responsive>
+        <thead className="thead-dark">
           <tr>
             <th>ID</th>
-            <th>NAME</th>
-            <th>EMAIL</th>
-            <th>ACTION</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((student, index) => {
-            console.log(student)
-            return (
-              <tr key={index}>
-                <td>{student.id}</td>
-                <td>{student.name}</td>
-                <td>{student.email}</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Replace</button>
-                </td>
-              </tr>
-            );
-          })}
+          {data.map((student, index) => (
+            <tr key={index}>
+              <td>{student.id}</td>
+              <td>{student.name}</td>
+              <td>{student.email}</td>
+              <td>
+                <Button
+                  variant="warning"
+                  size="sm"
+                  className="me-2"
+                  onClick={() => console.log("Edit", student.id)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="danger"
+                  size="sm"
+                  onClick={() => console.log("Replace", student.id)}
+                >
+                  Replace
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-      </table>
-    </>
+      </Table>
+    </Container>
   );
 };
 
