93e3510f49f4dd59f7b366927e34756e05f1e1db
Johno <john@mount-media-online.com>
2024-12-11 10:37:06 +0000

    err handling implemented
    auth My SQL

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import axios from 'axios'
-import { useEffect} from react
+import { useEffect, useState } from 'react'
 
 const Home = () => {
-
+  const [data, setdata] =  useState([]);
   useEffect(() => {
     axios.get('http://localhost:8081/')
-    .then(res => console.log(res))
+    .then(res => setdata(res.data))
     .catch(err => console.log(err))
   }, [])
   return ( 
-    <></>
+    <>
+    <table>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>ACTION</th>
+
+        </tr>
+      </tbody>
+    </table>
+    
+    </>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,25 +1,49 @@
-import express from 'express'
-import mysql from 'mysql'
-import cors from 'cors'
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json()); 
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password:"",
-    database:"my_STEM"
-})
+    password: "password_auth",
+    database: "my_STEM"
+});
 
-app.get('/', (req, res) => {
-    const sql = "SELECT * FROM student";
-    db.query(sql, (err, result) => {
-        if(err) return res.json({Message: "Error inside server"})
-        return res.json(result);
-    })
-})
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection error:', err);
+        process.exit(1); 
+    }
+    console.log('Connected to MySQL database');
+});
 
-app.listen (8081, ()=> {
-    console.log("listening")
-})
+app.get('/', async (req, res) => {
+    try {
+        const sql = "SELECT * FROM students";
+
+        const queryPromise = new Promise((resolve, reject) => {
+            db.query(sql, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        const result = await queryPromise;
+        res.json(result);
+
+    } catch (error) {
+        console.error('Error inside server:', error); 
+        res.status(500).json({ Message: "Error inside server", Error: error.message });
+    }
+});
+
+app.listen(8081, () => {
+    console.log("Server is running on http://localhost:8081");
+});
